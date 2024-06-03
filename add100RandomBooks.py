7c6d40134bebe1d424b7c25af7669c31a5630ca9
Adrien Capitaine <adrien.capitaine@epita.fr>
2024-06-03 01:11:02 +0000

    dsds

--- add100RandomBooks.py
+++ add100RandomBooks.py
@@ -3,6 +3,7 @@
 import requests
 import json
 from faker import Faker
+#POtentiql edit
 
 
 APIHOST = "http://library.demo.local"
@@ -34,15 +35,32 @@ def addBook(book, apiKey):
     else:
         raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
 
+
+def deleteBook(book, apiKey):
+    r = requests.delete(
+        f"{APIHOST}/api/v1/books", 
+        headers = {
+            "Content-type": "application/json",
+            "X-API-Key": apiKey
+            },
+        data = json.dumps(book)
+    )
+    if r.status_code == 200:
+        print(f"Book {book} added.")
+    else:
+        raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
+
+
+
 # Get the Auth Token Key
 apiKey = getAuthToken()
 
 # Using the faker module, generate random "fake" books
 fake = Faker()
-for i in range(4, 29):
+for i in range(1, 6):
     fakeTitle = fake.catch_phrase()
     fakeAuthor = fake.name()
     fakeISBN = fake.isbn13()
-    book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
+    book = {"id":i}
     # add the new random "fake" book using the API
-    addBook(book, apiKey) 
+    deleteBook(book, apiKey) 
